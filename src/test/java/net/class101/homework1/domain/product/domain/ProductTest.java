8d4f318619e1440c9d0421196f88d159c7d9b246
junggu <junggu.ji.dev@gmail.com>
2020-10-20 16:11:56 +0900

    type : Test
    test 생성

---
+++ src/test/java/net/class101/homework1/domain/product/domain/ProductTest.java
@@ -0,0 +1,12 @@
+package net.class101.homework1.domain.product.domain;
+
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProductTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+}
