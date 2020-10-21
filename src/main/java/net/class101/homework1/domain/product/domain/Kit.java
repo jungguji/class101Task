0eae0668dfecc3d5c5c0eb8ad26bb82853388d6c
JG.JI <junggu.ji.dev@gmail.com>
2020-10-21 14:49:37 +0900

    #2 type : feat
    * 기본 생서자 추가
    * @DiscriminatorValue 추가하여 상속 관계 명시

--- src/main/java/net/class101/homework1/domain/product/domain/Kit.java
+++ src/main/java/net/class101/homework1/domain/product/domain/Kit.java
@@ -1,15 +1,20 @@
 package net.class101.homework1.domain.product.domain;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
 
+@NoArgsConstructor
 @Getter
+@DiscriminatorValue(value = "KIT")
 @Entity
 public class Kit extends Product {
     private Integer stock;
 
-    public Kit(Integer stock) {
+    public Kit(String name, Integer price, Integer stock) {
+        super(name, price);
         this.stock = stock;
     }
 }
--- src/main/java/net/class101/homework1/domain/product/domain/Product.java
+++ src/main/java/net/class101/homework1/domain/product/domain/Product.java
@@ -1,9 +1,22 @@
 package net.class101.homework1.domain.product.domain;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import net.class101.homework1.domain.model.ProductType;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.DiscriminatorColumn;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
 
+@NoArgsConstructor
+@Getter
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE)
 @DiscriminatorColumn(name = "type")
 @Entity
@@ -18,4 +31,9 @@ public class Product {
     @Enumerated(EnumType.STRING)
     @Column(name ="product_type")
     private ProductType type;
+
+    public Product(String name, Integer price) {
+        this.name = name;
+        this.price = price;
+    }
 }
