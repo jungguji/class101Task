dfd19b79696b2dd856ad18c870c121fa043f92fd
junggu <junggu.ji.dev@gmail.com>
2020-10-23 13:57:10 +0900

    type : feat
    * 이미 주문된 Klass인지 체크하기 위해 Order에 type 변수 추가
    * 이미 주문된 Klass인 경우 추가되지 않도록 addOrder 수정
    * 직관성을 위해 getOrder 메서드명 -> getOrderOrCreate 로 변경

--- src/main/java/net/class101/homework1/domain/order/Order.java
+++ src/main/java/net/class101/homework1/domain/order/Order.java
@@ -2,20 +2,25 @@ package net.class101.homework1.domain.order;
 
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import net.class101.homework1.domain.model.ProductType;
 
+@NoArgsConstructor
 @Getter
 public class Order {
     private Integer id;
     private String name;
     private Integer amount;
     private Integer price;
+    private ProductType type;
 
     @Builder
-    public Order(Integer id, String name, Integer amount, Integer price) {
+    public Order(Integer id, String name, Integer amount, Integer price, ProductType type) {
         this.id = id;
         this.name = name;
         this.amount = amount;
         this.price = calculate(price);
+        this.type = type;
     }
 
     private int calculate(Integer price) {
--- src/main/java/net/class101/homework1/domain/order/OrderHistory.java
+++ src/main/java/net/class101/homework1/domain/order/OrderHistory.java
@@ -1,12 +1,14 @@
 package net.class101.homework1.domain.order;
 
+import net.class101.homework1.domain.model.ProductType;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class OrderHistory {
-    List<Order> orders = new ArrayList<>();
-    Integer orderPrice;
-    Integer shippingFee;
+    private List<Order> orders = new ArrayList<>();
+    private Integer orderPrice;
+    private Integer shippingFee;
 
     public OrderHistory() {
         this.orderPrice = 0;
@@ -17,20 +19,26 @@ public class OrderHistory {
         this.shippingFee = getShippingFee();
     }
 
-    public void addOrder(Order order) {
-        this.orders.add(order);
+    public boolean addOrder(Order order) {
+        for (Order o : this.orders) {
+            if (order.getId().equals(o.getId()) && ProductType.KLASS.equals(order.getType())) {
+                return false;
+            }
+        }
+        return this.orders.add(order);
     }
 
     private int getShippingFee() {
         return this.orderPrice < 50000 ? 5000 : 0;
     }
 
-    public Order getOrder(Integer id, String name) {
+    public Order getOrderOrCreate(Integer id, String name, ProductType type) {
         Order order = Order.builder()
                 .id(id)
                 .name(name)
-                .amount(0)
                 .price(0)
+                .amount(0)
+                .type(type)
                 .build();
 
         for (Order o : this.orders) {
