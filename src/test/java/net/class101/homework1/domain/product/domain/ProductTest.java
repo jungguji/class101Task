6634e615784dc518c3a46122e4228e5ed679b756
junggu <junggu.ji.dev@gmail.com>
2020-10-26 17:38:42 +0900

    type : test
    * synchronized가 product로 이동함에 따라 멀티 스레드 테스트 코드도 productTest로 이동

--- src/test/java/net/class101/homework1/domain/product/domain/ProductTest.java
+++ src/test/java/net/class101/homework1/domain/product/domain/ProductTest.java
@@ -7,6 +7,8 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.sql.SQLException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -48,5 +50,84 @@ class ProductTest {
 
         //then
         assertEquals(99999, given.getStock());
+    }
+
+
+    @Test
+    void update_multi_thread() throws InterruptedException {
+        //given
+        ExecutorService service = Executors.newFixedThreadPool(3);
+
+        Kit kit = new Kit("test", 1000, 100);
+        this.productRepository.save(kit);
+
+        Kit given = (Kit) productRepository.findById(kit.getId()).get();
+
+        //when
+        service.execute(() -> {
+            int count = 1;
+            while (kit.getStock() > 0) {
+                given.update(count);
+                System.out.println(Thread.currentThread().getName() + " - " + count + "개씩 빼는 스레드 / 남은 갯수 : " + given.getStock());
+                sleep();
+            }
+        });
+
+        service.execute(() -> {
+            int count = 3;
+            while (kit.getStock() > 0) {
+                given.update(count);
+                System.out.println(Thread.currentThread().getName() + " - " + count + "개씩 빼는 스레드 / 남은 갯수 : " + given.getStock());
+                sleep();
+            }
+        });
+
+        //when
+        service.execute(() -> {
+            int count = 32;
+            while (kit.getStock() > 0) {
+                given.update(count);
+                System.out.println(Thread.currentThread().getName() + " - " + count + "개씩 빼는 스레드 / 남은 갯수 : " + given.getStock());
+                sleep();
+            }
+        });
+
+        service.execute(() -> {
+            int count = 12;
+            while (kit.getStock() > 0) {
+                given.update(count);
+                System.out.println(Thread.currentThread().getName() + " - " + count + "개씩 빼는 스레드 / 남은 갯수 : " + given.getStock());
+                sleep();
+            }
+        });
+
+        service.execute(() -> {
+            int count = 3;
+            while (kit.getStock() > 0) {
+                given.update(count);
+                System.out.println(Thread.currentThread().getName() + " - " + count + "개씩 빼는 스레드 / 남은 갯수 : " + given.getStock());
+                sleep();
+            }
+        });
+
+        service.execute(() -> {
+            int count = 2;
+            while (kit.getStock() > 0) {
+                given.update(count);
+                System.out.println(Thread.currentThread().getName() + " - " + count + "개씩 빼는 스레드 / 남은 갯수 : " + given.getStock());
+                sleep();
+            }
+        });
+
+        Thread.sleep(10*1000);
+    }
+
+    public void sleep() {
+        try {
+            // 스레드 1초 대기
+            Thread.sleep(1);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
